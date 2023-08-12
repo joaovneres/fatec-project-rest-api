10aa9a554ebc51b4cbd55ef9c2788cefd285df8d
João Neres <joaovneres@outlook.com>
2023-08-12 16:19:06 -0300

    Aula 12/08

---
+++ src/main/java/br/com/fatec/FatecProjectRestAPI/entity/Customer.java
@@ -0,0 +1,17 @@
+package br.com.fatec.FatecProjectRestAPI.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Customer {
+    private Long idCustomer;
+    private String firstNameCustomer;
+    private String lastNameCustomer;
+    private String cpfCustomer;
+    private LocalDate birthDateCustomer;
+    private LocalDate dateCreatedCustomer;
+    private BigDecimal monthlyIncomeCustomer;
+    private Boolean statusCustomer;
+    private String emailCustomer;
+    private String passwordCustomer;
+}
